a3556af8135fa53a51fd59c38ef10744ffd11bec
CerfSoleil <robedj12@gmail.com>
2025-03-03 01:22:26 -0600

    Auth-routes added for JWT token

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -1,15 +1,40 @@
-import { Router, Request, Response } from 'express';
+import { Router, type Request, type Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+    const { username, password } = req.body;
+
+    const user = await User.findOne({
+        where: { username },
+    });
+    if (!user) {
+        return res.status(401).json({ message: 'User not found' });
+    }
+    console.log("Stored hash from DB:", user.password);
+    console.log("Entered password:", password);
+
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    console.log("Password match result:", passwordIsValid);
+
+    if (!passwordIsValid) {
+        return res.status(401).json({ message: 'Incorrect password' });
+    }
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    const token = jwt.sign({ id: user.id, username: user.username}, secretKey, { expiresIn: '1d' });
+    return res.json({ token,
+     });
 };
 
 const router = Router();
 
-// POST /login - Login a user
 router.post('/login', login);
 
 export default router;
+
