6f2a9b676494d2f17f0d340684fc0de15641c7c6
CerfSoleil <robedj12@gmail.com>
2025-03-03 03:32:49 -0600

    connect to postgresql

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,15 +5,32 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+// The following is to code for Render
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize =new Sequelize(
+    process.env.DB_NAME || 'default_db_name',
+    process.env.DB_USER || 'default_db_user',
+    process.env.DB_PASSWORD,
+    {
       host: 'localhost',
       dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    },
+  );
+}
+
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
